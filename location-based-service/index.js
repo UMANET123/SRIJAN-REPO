61af9fce44d0751626d4fd0aa4029abdd253b272
Valindo Godinho <valindo.godinho@gmail.com>
2018-09-24 13:55:09 +0530

    Made some lint changes

--- location-based-service/index.js
+++ location-based-service/index.js
@@ -8,91 +8,93 @@ const port = process.env.PORT_NUMBER;
 
 app.use(xmlparser());
 
-// Logger
+// Add logic to manage verbosity in prod vs dev env
 if (process.env.NODE_ENV == 'prod') {
-    app.use(morgan('common')); // Add Logic to maintain a logfile in the prod server
+    app.use(morgan('common')); // Less Verbose ( Uses Apache Style Logs <DATE> Log details)
 } else {
     app.use(morgan('dev'));
 }
 
 
-// Middleware to only accept POST requests
+// Middleware to ONLY accept POST requests
 app.use((req, res, next) => {
     if (req.method != "POST") {
-        res.sendStatus(405)
-        res.end();
+        res.sendStatus(405);
+        return res.end();
     }
     next();
 })
 
-// Middleware to Check if Soap Contract is present
+// Middleware to Check if Soap Contract is present in the request body
 app.use((req, res, next) => {
     if (Object.keys(req.body).length == 0 && req.method == "POST") {
-        res.sendStatus(400)
-        return res.send()
+        res.sendStatus(400);
+        return res.send();
     }
     next();
 })
 
+/**
+ * MSISDN = Mobile Station International Subscriber Directory Number
+ */
 app.post('/', (req, res, next) => {
 
-    const body = req.body
+    const body = req.body;
     const {
         "soap:envelope": {
             "soap:body": [{
                 "loc:getlocationbymsisdn": [{
-                    "csp_txid": [txid],
-                    "cp_id": [cpid],
-                    "cp_userid": [cpuserid],
-                    "cp_password": [cppassword],
-                    "serviceid": [serviceid],
-                    "productid": [productid],
                     "msisdn": [msisdn]
                 }]
             }]
         }
     } = body;
 
-
-
+    /*
+     ** Test if number is from the Philippines or not ( Regex )
+     */
     if (!(/^639[0-9]{9}$/.test(msisdn))) {
         res.sendStatus(400);
-        return res.send();
+        return res.send({
+            error: "MSISDN Invalid, does not belong to the Philippines"
+        });
     }
+
     let data = `<soap:Envelope
-    xmlns:soap="http://www.w3.org/2003/05/soap-envelope"
-    xmlns:head="http://www.globe.com/warcraft/wsdl/header/"
-    xmlns:loc="http://www.globe.com/warcraft/wsdl/locationmgt/">
-    <soap:Header>
-        <head:WarcraftHeader>
-            <!--Optional:-->
-            <EsbMessageID>12345678</EsbMessageID>
-            <!--Optional:-->
-            <EsbRequestDateTime>1234567890</EsbRequestDateTime>
-            <!--Optional:-->
-            <EsbResponseDateTime>${Date.now()}</EsbResponseDateTime>
-            <!--Optional:-->
-            <EsbIMLNumber>123456789</EsbIMLNumber>
-            <!--Optional:-->
-            <EsbOperationName>1234567890</EsbOperationName>
-        </head:WarcraftHeader>
-    </soap:Header>
-    <soap:Body>
-        <loc:GetLocationByMSISDNResponse>
-            <!--Optional:-->
-            <GetLocationByMSISDNResult>
-                <!--Optional:-->
-                <MSISDN>${msisdn}</MSISDN>
-                <!--Optional:-->
-                <Latitude>1234567890</Latitude>
-                <!--Optional:-->
-                <Longitude>1234567890</Longitude>
-            </GetLocationByMSISDNResult>
-        </loc:GetLocationByMSISDNResponse>
-    </soap:Body>
-</soap:Envelope>`
-    res.type('application/xml')
-    res.send(data)
+                    xmlns:soap="http://www.w3.org/2003/05/soap-envelope"
+                    xmlns:head="http://www.globe.com/warcraft/wsdl/header/"
+                    xmlns:loc="http://www.globe.com/warcraft/wsdl/locationmgt/">
+                    <soap:Header>
+                        <head:WarcraftHeader>
+                            <!--Optional:-->
+                            <EsbMessageID>12345678</EsbMessageID>
+                            <!--Optional:-->
+                            <EsbRequestDateTime>1234567890</EsbRequestDateTime>
+                            <!--Optional:-->
+                            <EsbResponseDateTime>${Date.now()}</EsbResponseDateTime>
+                            <!--Optional:-->
+                            <EsbIMLNumber>123456789</EsbIMLNumber>
+                            <!--Optional:-->
+                            <EsbOperationName>1234567890</EsbOperationName>
+                        </head:WarcraftHeader>
+                    </soap:Header>
+                    <soap:Body>
+                        <loc:GetLocationByMSISDNResponse>
+                            <!--Optional:-->
+                            <GetLocationByMSISDNResult>
+                                <!--Optional:-->
+                                <MSISDN>${msisdn}</MSISDN>
+                                <!--Optional:-->
+                                <Latitude>1234567890</Latitude>
+                                <!--Optional:-->
+                                <Longitude>1234567890</Longitude>
+                            </GetLocationByMSISDNResult>
+                        </loc:GetLocationByMSISDNResponse>
+                    </soap:Body>
+                </soap:Envelope>`;
+
+    res.type('application/xml');
+    res.send(data);
 });
 
 app.listen(port, () => {
