270aed0206b6e168f534f70c99f660f3f2dcb72d
Valindo Godinho <valindo.godinho@gmail.com>
2018-09-26 09:39:48 +0530

    Updated the comments for the index file

--- location-based-service/index.js
+++ location-based-service/index.js
@@ -1,41 +1,81 @@
-// Imports
+/**
+ * Location Based Service Mock Micro Service
+ * 
+ * This service is written to mock the current location based service deployed at globe
+ * 
+ * This accepts soap contracts which it parses into json, picks out the required data and sends
+ * back a soap contract
+ */
+
+/**
+ * Dependencies :
+ * 1. Express
+ * 2. Xmlparser
+ * 3. Morgan
+ */
+
 const express = require('express');
 const xmlparser = require('express-xml-bodyparser');
 const morgan = require('morgan');
-// Setup
+
+
 const app = express();
 const port = process.env.PORT_NUMBER;
 
+/**
+ * Middleware Xmlparser to parse XML into JSON
+ * 
+ * Soap Contracts are written in XML , which need to be parsed into json to work with
+ */
 app.use(xmlparser());
 
-// Add logic to manage verbosity in prod vs dev env
+/**
+ * This set's the level of logging based on the environment
+ * 
+ * Production = morgan('common') , Less Verbose ( Uses Apache Style Logs <DATE> Log Details)
+ * Development = morgan('dev') More Verbose
+ */
 if (process.env.NODE_ENV == 'prod') {
-    app.use(morgan('common')); // Less Verbose ( Uses Apache Style Logs <DATE> Log details)
+    app.use(morgan('common'));
 } else {
     app.use(morgan('dev'));
 }
 
 
-// Middleware to ONLY accept POST requests
+/**
+ * Middleware to check that only POST requests are allowed
+ * 
+ * Returns status 405
+ */
 app.use((req, res, next) => {
     if (req.method != "POST") {
-        res.sendStatus(405);
-        return res.end();
-    }
-    next();
-})
-
-// Middleware to Check if Soap Contract is present in the request body
-app.use((req, res, next) => {
-    if (Object.keys(req.body).length == 0 && req.method == "POST") {
-        res.sendStatus(400);
-        return res.send();
+        return res.status(405).send();
     }
     next();
 })
 
 /**
- * MSISDN = Mobile Station International Subscriber Directory Number
+ * Middleware to check the presence of the SOAP contract in the body
+ * 
+ * Returns status 400
+ */
+app.use((req, res, next) => {
+    if (Object.keys(req.body).length == 0 && req.method == "POST") {
+        return res.status(400).send();
+    }
+    next();
+})
+
+/*
+ * Handler to process SOAP contract and and return new SOAP contract
+ * 
+ * Accepts SOAP contract and returns a new SOAP contract
+ * 
+ * Validates MSISDN - Mobile Number if it belong to the Philippines or not
+ * 
+ * returns 200 for success
+ * 
+ * returns 400 in case the Number (msisdn) is not valid
  */
 app.post('/', (req, res, next) => {
 
@@ -50,12 +90,14 @@ app.post('/', (req, res, next) => {
         }
     } = body;
 
-    /*
-     ** Test if number is from the Philippines or not ( Regex )
+    /**
+     * Regex to check if number belongs to the Philippines or not
+     * 
+     * Returns 400 if it fails, else continues
      */
+
     if (!(/^639[0-9]{9}$/.test(msisdn))) {
-        res.sendStatus(400);
-        return res.send({
+        return res.status(400).send({
             error: "MSISDN Invalid, does not belong to the Philippines"
         });
     }
