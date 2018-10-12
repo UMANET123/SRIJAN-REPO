3182e7bdfabda2ea32aa09ca7c061b468b3cee3a
Valindo Godinho <valindo.godinho@gmail.com>
2018-10-12 08:19:41 +0530

    Fixed the bug for scope

--- oauth/routes/consent.js
+++ oauth/routes/consent.js
@@ -4,11 +4,11 @@ var utils = require("../lib/utils");
 exports.get = function(req, res) {
   var data = {
     client_id: req.query.client_id,
-    scope: req.query.scope,
+    scope: req.query.scope.split(","),
     state: req.query.state,
     redirect_uri: req.query.redirect_uri,
     appName: req.query.appName,
-    appProducts: req.query.appProducts.split(",")
+    appProducts: req.query.appProducts
   };
 
   var basePath = utils.getBasePath(req);
