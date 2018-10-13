822f5e8357d493e7d838eb8d4e1549c308866fe4
Valindo Godinho <valindo.godinho@gmail.com>
2018-10-14 00:43:33 +0530

    Got the login Flow to work Got the registration flow to work, with a hash generation and presistance Got the verification flow to work, which pulls data from redis to confirm Got the regneration of verification hash working Incorporated Redis to persist verification data

--- identity/server/index.js
+++ identity/server/index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const passport = require("passport");
+const crypto = require('crypto');
 const bcrypt = require("bcrypt");
 const morgan = require("morgan");
 const app = express();
+const redis = require('redis');
+const client = redis.createClient('redis://emailtokens');
+
+client.on('connect', (err) => {
+  console.log("connected to Redis server");
+})
 
 app.use(bodyParser.json());
-morgan.token("request-body", function(req, res) {
+morgan.token("request-body", function (req, res) {
   return JSON.stringify(req.body);
 });
 
@@ -17,6 +23,7 @@ app.use(
 );
 
 const saltRounds = 10;
+const emailVerifySaltRounds = 15;
 let mockData = [];
 
 bcrypt.hash("baconpancakes", saltRounds, (err, hash) => {
@@ -44,24 +51,30 @@ app.post("/register", (req, res) => {
     });
   }
 
-  if (!req.body.password.length < 6) {
+  if (req.body.password.length < 6) {
     return res.status(400).send({
       message: "Please enter a password of length greater than 6"
     });
   }
 
   bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-    mockData.push({
-      firstname: req.body.firstname,
-      middlename: req.body.middlename,
-      lastname: req.body.lastname,
-      address: req.body.address,
-      msisdn: req.body.msisdn,
-      email: req.body.email,
-      password: hash
-    });
-    res.status(201).send({
-      message: "Account Created"
+    crypto.randomBytes(48, function (err, buffer) {
+      var token = buffer.toString('hex');
+      mockData.push({
+        firstname: req.body.firstname,
+        middlename: req.body.middlename,
+        lastname: req.body.lastname,
+        address: req.body.address,
+        msisdn: req.body.msisdn,
+        email: req.body.email,
+        password: hash,
+        emailVerify: false
+      });
+      client.set(req.body.email, token, 'EX', 180)
+      res.status(201).send({
+        email: req.body.email,
+        hash: token
+      });
     });
   });
 });
@@ -76,9 +89,16 @@ app.post("/login", (req, res) => {
         mockData[i].password,
         (err, isValid) => {
           if (isValid) {
-            return res.status(200).send({
-              message: "Successfully Logged in"
-            });
+            if (mockData[i].emailVerify) {
+              return res.status(200).send({
+                message: "Successfully Logged in"
+              });
+            } else {
+              return res.status(401).send({
+                message: "Please Verify your email to continue, if you have not recieved a verficiation email, request for a new one"
+              });
+            }
+
           } else {
             return res.status(401).send({
               message: "Incorrect password or email"
@@ -95,6 +115,46 @@ app.post("/login", (req, res) => {
     });
   }
 });
+
+app.post('/verify*', (req, res) => {
+  let email = req.body.email;
+  let hash = req.body.hash;
+  client.get(email, (err, storedHash) => {
+    if (storedHash) {
+      if (storedHash == hash) {
+        mockData = mockData.map(data => {
+          if (data.email == email) {
+            data.emailVerify = true
+          }
+          return data
+        })
+        return res.status(200).send({
+          message: "Verification Complete, please login to continue"
+        })
+      } else {
+        return res.status(400).send({
+          message: "Malformed Verification"
+        })
+      }
+    } else {
+      return res.status(400).send({
+        message: "Verification Window Expired, please request for a new verification email "
+      })
+    }
+  })
+})
+
+app.post('/regenerate', (req, res) => {
+  let email = req.body.email;
+  crypto.randomBytes(48, function (err, buffer) {
+    var token = buffer.toString('hex');
+    client.set(email, token)
+    res.status(201).send({
+      email: email,
+      hash: token
+    })
+  })
+})
 
 function checkEmailAddress(email) {
   let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
