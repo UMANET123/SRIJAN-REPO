fcf15f7b2247cf444f00d2dbaa959a3085c14563
Ravindra Singh <it.ravindrasingh@gmail.com>
2019-02-06 18:51:27 +0530

    Added postgres connection to the code.

--- src/micro-services/auth-service/app.js
+++ src/micro-services/auth-service/app.js
@@ -14,12 +14,33 @@ db.create("redis://authredis", (error) => {
         console.log('Redis Server Running');
     }
 });
-
+console.log('db is on the way')
 app.use(bodyParser.json());
 app.use(httpPostOnlyMiddleware);
 app.use('/', router)
 app.listen(environment.PORT_NUMBER, () => {
     console.log(`App running on port ${environment.PORT_NUMBER}`);
 });
+
+const pg = require('pg');
+
+var config = {
+    user: "wakanda_auth_service",
+    database: "wakanda_auth_db",
+    host: "db",
+    password: "12345",
+    port: "5432"
+  };
+
+
+console.log("config : "+JSON.stringify(config));
+var pool = new pg.Pool(config);
+pool.query('SELECT NOW()', function(err, res) {
+    if (err) throw err
+    console.log(res.rows)
+  })
+// pg.connect("postgres://wakanda_auth_service:12345@db:5432/wakanda_auth_db");
+
+
 
 module.exports = app;
