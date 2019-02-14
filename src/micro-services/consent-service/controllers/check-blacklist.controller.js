f340238f57a41bbe96256c4d693170279a9da129
tejomayonline <tejomay.saha@srijan.net>
2019-02-14 12:52:16 +0530

    create blacklist , check blacklist added

--- src/micro-services/consent-service/controllers/check-blacklist.controller.js
+++ src/micro-services/consent-service/controllers/check-blacklist.controller.js
@@ -1,14 +1,8 @@
 const {checkBlacklist} = require('../models/identity.model');
 
 module.exports = (req, res) => {
-    let {subscriber_id, app_id} = req.params;
-    if ( !subscriber_id || !app_id ) {
-        return res.status(400).send({
-            "error_code": "BadRequest",
-            "error_message": "Bad Request"
-          });
-    }
-    checkBlacklist(req.body, (status, response) => {
+    //  check app is blacklisted
+    checkBlacklist(req.params, (status, response) => {
         return res.status(status).send(response);
     });
 }
--- src/micro-services/consent-service/controllers/create-blacklist.controller.js
+++ src/micro-services/consent-service/controllers/create-blacklist.controller.js
@@ -1,13 +1,16 @@
 const {createBlackList} = require('../models/identity.model');
 
 module.exports = (req, res) => {
+    //  get  required elements  from request body
     let {subscriber_id, app_id , developer_id} = req.body;
+    //  reject the response for absense of any required elements
     if ( !subscriber_id || !app_id || ! developer_id) {
         return res.status(400).send({
             "error_code": "BadRequest",
             "error_message": "Bad Request"
           });
     }
+    //  create a black list app record
     createBlackList(req.body, (status, response) => {
         return res.status(status).send(response);
     });
