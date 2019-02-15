0ba9b461d44e77cdf979f5302c95ff54ed227842
tejomayonline <tejomay.saha@srijan.net>
2019-02-15 15:10:26 +0530

    get subscriber apps api created

---
+++ src/micro-services/consent-service/controllers/get-subscriber-apps.controller.js
@@ -0,0 +1,8 @@
+const {getSubscriberApps} = require('../models/subscriber.model');
+module.exports = (req, res) => {
+    let {subscriber_id} = req.params;
+    let {appname} = req.query;
+    getSubscriberApps(subscriber_id, appname, (status, response)=>{
+        res.status(status).send(response);
+    });
+}
---
+++ src/micro-services/consent-service/models/subscriber.model.js
@@ -0,0 +1,35 @@
+const pool = require('../config/db');
+//  get subscriber apps
+function getSubscriberApps(subscriber_id, appname, callback) {
+    (async () => {
+        const client = await pool.connect();
+        try {
+            let record = null;
+            if (appname) {
+                //  get all whitelisted apps of a subscriber with appname enterred
+              record = await client.query("SELECT appname FROM public.subscriber_consent consent inner join apps_metadata app on consent.app_id=app.app_id and consent.developer_id=app.developer_id where uuid=($1) and status=($2) and scopes IS NOT null and lower(appname) Like ($3)", [subscriber_id, 0, `%${appname.toLowerCase()}%`]);
+            } else {
+                //  get all whitelisted apps of a subscriber without appname
+              record = await client.query("SELECT appname FROM public.subscriber_consent consent inner join apps_metadata app on consent.app_id=app.app_id and consent.developer_id=app.developer_id where uuid=($1) and status=($2) and scopes IS NOT null", [subscriber_id, 0]);
+            }
+            console.log({res: record.rows});
+            if (record.rows[0]) {
+              let appArray = record.rows.map(({appname}) => appname);
+              callback(200, { "appname": appArray});  
+            } else {
+              callback(400, {
+                "error_code": "BadRequest",
+                "error_message": "Bad Request"
+              });
+            }
+                
+        } finally {
+          client.release();
+        }
+      })().catch(e => {
+          console.log(e.stack)
+          throw e;
+        });
+}
+
+module.exports = {getSubscriberApps};
