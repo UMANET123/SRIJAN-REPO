83bc85443a1fb3ae1a8fafec05aefae8160c3b49
tejomayonline <tejomay.saha@srijan.net>
2019-02-20 13:08:40 +0530

    validate transaction api added

--- src/micro-services/auth-service/models/transaction.model.js
+++ src/micro-services/auth-service/models/transaction.model.js
@@ -9,14 +9,20 @@ function validateTransaction(...args) {
         const client = await pool.connect();
         try {
           // insert transaction record
-          await client.query("INSERT INTO transaction_data(transaction_id, uuid, app_id, created, status) values($1, $2, $3, $4, $5)", [txnId, subscriberId, appId, currentDate, status]);
-          callback(txnId);
+          let txnRecord = await client.query("SELECT * FROM transaction_data WHERE transaction_id=($1) and uuid=($2) and app_id=($3) and status=($4)", [transaction_id, subscriber_id, app_id, 0]);
+          let txnValid = false;
+          if(txnRecord.rows[0]) {
+            txnValid = true;
+          } 
+          callback(200, {
+            "is_valid": txnValid
+          });
+
        } finally {
           client.release();
         }
       })().catch(e =>{
         console.log(e.stack);
-        callback(false);
       } );
   
   }
