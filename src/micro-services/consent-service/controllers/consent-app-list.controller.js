f4f01538c71a5694e35578fc61f9390f308ffe42
Valindo Godinho <valindo.godinho@gmail.com>
2019-03-04 09:46:47 +0530

    Added filter by date to the sql queries

--- src/micro-services/consent-service/controllers/consent-app-list.controller.js
+++ src/micro-services/consent-service/controllers/consent-app-list.controller.js
@@ -2,8 +2,8 @@ const {getConsentList} = require('../models/consent.model');
 
 module.exports = (req, res) => {
     let {subscriber_id} = req.params;
-    let {limit, page, appname} = req.query;
-    getConsentList(subscriber_id, limit, page, appname, (status, response) => {
+    let {limit, page, appname, order} = req.query;
+    getConsentList(subscriber_id, limit, page, appname,order, (status, response) => {
         return res.status(status).send(response);
     });
 };
