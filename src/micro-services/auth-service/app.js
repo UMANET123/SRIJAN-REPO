cea7abdce61fe6cf42e708537ce667dc52dd4c21
tejomayonline <tejomay.saha@srijan.net>
2019-03-07 23:27:17 +0530

    generate-otp model refactoed with controller, orm test route updated

--- src/micro-services/auth-service/app.js
+++ src/micro-services/auth-service/app.js
@@ -1,17 +1,19 @@
-const express = require('express');
-const router = require('./routes/index.router');
-const {NODE_SETTINGS:{portNumber}, AUTH_BASE_PATH} = require('./config/environment');
+const express = require("express");
+const router = require("./routes/index.router");
+const {
+  NODE_SETTINGS: { portNumber },
+  AUTH_BASE_PATH
+} = require("./config/environment");
 const app = express();
 
 // const httpPostOnlyMiddleware = require('./middleware/http-only-post.middleware');
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 // app.use(httpPostOnlyMiddleware);
 app.use(AUTH_BASE_PATH, router);
 app.listen(portNumber, () => {
-    console.log(`App running on port ${portNumber}`);
+  console.log(`App running on port ${portNumber}`);
 });
-
 
 module.exports = app;
--- src/micro-services/auth-service/models/helper.model.js
+++ src/micro-services/auth-service/models/helper.model.js
@@ -1,18 +1,24 @@
-require('dotenv').config();
-const crypto = require('crypto');
-const otplib = require('otplib');
-const axios = require('axios');
-const {OTP_SETTINGS:{timer, step} } = require('../config/environment');
-const {verifyUser} = require('./auth.model');
-const pool = require('../config/db');
-const addMinToDate = require('../helpers/add-minute-to-date');
+/*jshint esversion: 6 */
+require("dotenv").config();
+const crypto = require("crypto");
+const otplib = require("otplib");
+const axios = require("axios");
+const {
+  OTP_SETTINGS: { timer, step }
+} = require("../config/environment");
+const { verifyUser } = require("./auth.model");
+const pool = require("../config/db");
+const addMinToDate = require("../helpers/add-minute-to-date");
 //  create new otp
 function getNewOtp(secret) {
-    return otplib.authenticator.generate(secret);
+  return otplib.authenticator.generate(secret);
 }
-  // create new secret
+// create new secret
 function getNewSecret(msisdn) {
-    return crypto.createHash('md5').update(msisdn).digest('hex');
+  return crypto
+    .createHash("md5")
+    .update(msisdn)
+    .digest("hex");
 }
 
 /**
@@ -20,72 +26,44 @@ function getNewSecret(msisdn) {
  * - Better naming convention
  */
 function setOtpSettings() {
-    otplib.totp.options = {
-        step: step,
-        window: timer
-    };
-}
-// insert query transaction for totp for /generate/totp endpoint
-function insertOtpRecord({secret, otp, msisdn, app_id}) {
-    //  create a record for mask table  
-    (async () => {
-        const client = await pool.connect();
-        try {
-            let currentDate = new Date();
-            //  check for any record exists with the phone
-            let mask= await client.query(`SELECT uuid FROM subscriber_data_mask WHERE phone_no=($1)`, [msisdn]);
-            if (!mask.rows[0]) {
-              //  insert record to mask table
-              await client.query(`INSERT INTO subscriber_data_mask(uuid, phone_no, created, status) values($1, $2, $3, $4)`,
-              [secret, msisdn, currentDate, 0]);
-              console.log('mask table record created');
-            } else {
-              //  update secret as existing uuid 
-              secret = mask.rows[0].uuid;
-            }
-            // insert new otp record
-
-           await client.query(`INSERT INTO subscriber_otps(uuid, app_id, otp, expiration,                    status) values($1, $2, $3, $4, $5)`,
-            [secret, app_id, otp, addMinToDate(currentDate, 5), 0]);
-            // await client.query(`DELETE FROM flood_control WHERE uuid=($1) AND app_id=($2)`,[secret,app_id]);
-            // await client.query(`INSERT INTO flood_control(uuid, app_id) values($1, $2)`,[secret,app_id]);
-            await client.query(`DELETE FROM flood_control WHERE uuid=($1)`,[secret]);
-            await client.query(`INSERT INTO flood_control(uuid) values($1)`,[secret]);
-
-        } finally {
-            client.release();
-        }
-    })().catch(e => {
-      console.log(e.stack);
-      return Error;
-    });
-  
+  otplib.totp.options = {
+    step: step,
+    window: timer
+  };
 }
 
 //  check app-subsubser blacklisted
-function  checkBlackListApp({msisdn, app_id}, callback) {
-
-    let consent_base_url= process.env.CONSENT_SERVICE_BASEPATH;
-    // get uuid from phone
-    verifyUser(msisdn, null, (response) => {
-        if (response && response.uuid) {
-            //  do a query to check blacklist api with uuid and msisdn
-            let reqUrl=`${consent_base_url}/blacklist/${response.uuid}/${app_id}`;
-            axios.get(reqUrl)
-                .then(({data}) =>{
-                   if (data) {
-                      return callback({
-                        "error_code": "Forbidden",
-                        "error_message": "App is blacklisted"
-                      },
-                       403); 
-                   } 
-                })
-                .catch(function (error) {
-                     console.log(error);
-                });
-        }
-    });
+function checkBlackListApp({ msisdn, app_id }, callback) {
+  let consent_base_url = process.env.CONSENT_SERVICE_BASEPATH;
+  // get uuid from phone
+  verifyUser(msisdn, null, response => {
+    if (response && response.subscriber_id) {
+      //  do a query to check blacklist api with uuid and msisdn
+      let reqUrl = `${consent_base_url}/blacklist/${
+        response.subscriber_id
+      }/${app_id}`;
+      axios
+        .get(reqUrl)
+        .then(({ data }) => {
+          console.log({ data });
+          if (data) {
+            return callback(true);
+          } else {
+            return callback(false);
+          }
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    } else {
+      return callback(false);
+    }
+  });
 }
 
-module.exports = {getNewOtp, getNewSecret, setOtpSettings, insertOtpRecord, checkBlackListApp};
+module.exports = {
+  getNewOtp,
+  getNewSecret,
+  setOtpSettings,
+  checkBlackListApp
+};
