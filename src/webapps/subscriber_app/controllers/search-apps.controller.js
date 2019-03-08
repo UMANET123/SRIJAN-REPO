86a653ffc885a9cbe6fb5086e9167bd3e3c10ec3
tejomayonline <tejomay.saha@srijan.net>
2019-03-08 17:14:03 +0530

    Working on app search feature integration with frontend

--- src/webapps/subscriber_app/controllers/search-apps.controller.js
+++ src/webapps/subscriber_app/controllers/search-apps.controller.js
@@ -1,45 +1,49 @@
+const {
+  NODE_SETTINGS,
+  APIGEE_CREDS: { apigeeBaseURL },
+  APIGEE_CREDS: { clientID },
+  APIGEE_CREDS: { clientSecret },
+  APIGEE_ENDPOINTS: { searchApps }
+} = require("../config/environment");
 
+var request = require("request");
+var session = require("express-session");
+module.exports = function(req, res, next) {
+  sess = req.session;
 
-const { NODE_SETTINGS, APIGEE_CREDS: { apigeeBaseURL }, APIGEE_CREDS: { clientID }, APIGEE_CREDS: { clientSecret }, APIGEE_ENDPOINTS: { searchApps } } = require("../config/environment")
+  let sub_access_token = sess.access_token;
+  var authorizationHeaderString = "Bearer " + sub_access_token;
+  let { appname } = req.query;
+  let reqUrl = `${apigeeBaseURL}/${searchApps}`;
+  if (appname) reqUrl = `${reqUrl}?appname=${appname}`;
+  console.log({ reqUrl });
+  var options = {
+    method: "GET",
+    url: reqUrl,
+    headers: {
+      "cache-control": "no-cache",
+      Authorization: authorizationHeaderString,
+      "Content-Type": "application/json"
+    }
+  };
 
-var request = require('request');
-var session = require("express-session")
-module.exports = function (req, res, next) {
-	sess = req.session;
-	let sub_access_token = sess.access_token
-	var authorizationHeaderString = 'Bearer ' + sub_access_token;
-	var options = {
-		method: 'GET',
-		url: `${apigeeBaseURL}/${searchApps}`,
-		headers:
-		{
-			'cache-control': 'no-cache',
-			Authorization: authorizationHeaderString,
-			'Content-Type': 'application/json'
-		},
-	};
-	console.log(options)
-	request(options, function (error, response, body) {
-		if (error) throw new Error(error);
-		console.log(response.statusCode)
-		console.log()
+  request(options, function(error, response, body) {
+    if (error) throw new Error(error);
+    // console.log(response.statusCode);
+    // console.log();
 
-		var res_data = {}
-		sess = req.session
-		res_data.statusCode = response.statusCode
-		console.log(response.body)
-		if (response.statusCode == 200) {
-			body_data = JSON.parse(body)  
+    var res_data = {};
+    sess = req.session;
+    res_data.statusCode = response.statusCode;
+    // console.log(response.body);
+    if (response.statusCode == 200) {
+      body_data = JSON.parse(body);
 
-			res_data.appname = body_data['appname']
-		}
-		else {
-			res_data.error_message = 'There are some error during perform operations.'
-		}
-		res.send(res_data)
-
-	});
-
-}
-
-
+      res_data.appname = body_data["appname"];
+    } else {
+      res_data.error_message =
+        "There are some error during perform operations.";
+    }
+    res.send(res_data);
+  });
+};
--- src/webapps/subscriber_app/public/js/dashboard.js
+++ src/webapps/subscriber_app/public/js/dashboard.js
@@ -1,200 +1,239 @@
-
-$(document).ready(function () {
-
-  acSubscriberApps();
-  $('#revoke_all').click(function (event) {
+/*jshint esversion: 6 */
+$(document).ready(function() {
+  let app_search_target = "#subscriber_apps";
+  $(app_search_target)
+    .off("keyup")
+    .on("keyup", e => {
+      let search_element = e.target.value;
+      console.log({ search_element });
+      autoCompleteSearch(search_element);
+    });
+  $("#revoke_all").click(function(event) {
     event.preventDefault();
-    dialogRevokeAll()
+    dialogRevokeAll();
   });
 
-  $('.revoke_app').click(function (event) {
-
-   // var parent = $(this).closest('.subscribers_apps');
-    var dev_id = $(this).closest('.subscribers_apps').find('.developer_id').attr('id');
-    var app_id =  $(this).closest('.subscribers_apps').find('.app_id').attr('id');
-    var appname =  $(this).closest('.subscribers_apps').find('.app_id').text()
-    event.preventDefault();
-    dialogRevokeApp(dev_id,app_id,appname)
-  });
-  $('.blacklist_app').click(function (event) {
+  $(".revoke_app").click(function(event) {
     // var parent = $(this).closest('.subscribers_apps');
-    var dev_id = $(this).closest('.subscribers_apps').find('.developer_id').attr('id');
-    var app_id =  $(this).closest('.subscribers_apps').find('.app_id').attr('id');
-    var appname =  $(this).closest('.subscribers_apps').find('.app_id').text()
+    var dev_id = $(this)
+      .closest(".subscribers_apps")
+      .find(".developer_id")
+      .attr("id");
+    var app_id = $(this)
+      .closest(".subscribers_apps")
+      .find(".app_id")
+      .attr("id");
+    var appname = $(this)
+      .closest(".subscribers_apps")
+      .find(".app_id")
+      .text();
     event.preventDefault();
-    dialogBlacklist(dev_id,app_id,appname)
+    dialogRevokeApp(dev_id, app_id, appname);
+  });
+  $(".blacklist_app").click(function(event) {
+    // var parent = $(this).closest('.subscribers_apps');
+    var dev_id = $(this)
+      .closest(".subscribers_apps")
+      .find(".developer_id")
+      .attr("id");
+    var app_id = $(this)
+      .closest(".subscribers_apps")
+      .find(".app_id")
+      .attr("id");
+    var appname = $(this)
+      .closest(".subscribers_apps")
+      .find(".app_id")
+      .text();
+    event.preventDefault();
+    dialogBlacklist(dev_id, app_id, appname);
   });
 
   function dialogRevokeAll() {
     $.confirm({
       backgroundDismiss: true,
-      escapeKey: 'cancel',
-      title: 'Revoke All apps!',
-      content: '<p>This action will revoke the permissions from all installed apps.</p>' +
-        '<p>Are you sure you want to revoke permissions from all the apps?</p>',
+      escapeKey: "cancel",
+      title: "Revoke All apps!",
+      content:
+        "<p>This action will revoke the permissions from all installed apps.</p>" +
+        "<p>Are you sure you want to revoke permissions from all the apps?</p>",
       buttons: {
         yes: {
-          text: 'Yes',
-          btnClass: 'btn-blue',
-          keys: ['enter', 'shift'],
-          action: function () {
+          text: "Yes",
+          btnClass: "btn-blue",
+          keys: ["enter", "shift"],
+          action: function() {
             let self = this;
             $.ajax({
-              type : "POST",
-              contentType : "application/json",
-              url : "/api/revokeallapps",
-              data : '',
-              dataType : 'json',
-              success : function(response) {
-                console.log(response)
-                if(response['statusCode'] == 200){
-                  console.log({status: response['statusCode'] });
-                  self.setContentPrepend('<p class="success">Apps have been revoked successfully.</p>');
+              type: "POST",
+              contentType: "application/json",
+              url: "/api/revokeallapps",
+              data: "",
+              dataType: "json",
+              success: function(response) {
+                console.log(response);
+                if (response["statusCode"] == 200) {
+                  console.log({ status: response["statusCode"] });
+                  self.setContentPrepend(
+                    '<p class="success">Apps have been revoked successfully.</p>'
+                  );
                 } else {
-                  self.setContentPrepend('<p class="error">There is an error occured during the operation.</p>');
+                  self.setContentPrepend(
+                    '<p class="error">There is an error occured during the operation.</p>'
+                  );
                 }
-                setTimeout(() => {window.location.reload();},1000);
+                setTimeout(() => {
+                  window.location.reload();
+                }, 1000);
               },
-              error : function(e) {
+              error: function(e) {
                 console.log("ERROR: ", e);
               }
             });
             return false;
           }
         },
-        cancel: function () {
-        }
+        cancel: function() {}
       }
     });
   }
 
-  function dialogRevokeApp(dev_id,app_id,appname) {
+  function dialogRevokeApp(dev_id, app_id, appname) {
     $.confirm({
       backgroundDismiss: true,
-      escapeKey: 'cancel',
-      title: 'Revoke App',
-      content: '<p>This action will revoke all the permissions from this app</p>' +
-        '<p>Are you sure you want to revoke all the permission from this app?</p>',
+      escapeKey: "cancel",
+      title: "Revoke App",
+      content:
+        "<p>This action will revoke all the permissions from this app</p>" +
+        "<p>Are you sure you want to revoke all the permission from this app?</p>",
       buttons: {
         yes: {
-          text: 'Yes',
-          btnClass: 'btn-blue',
-          keys: ['enter', 'shift'],
-          action: function () {
-            
+          text: "Yes",
+          btnClass: "btn-blue",
+          keys: ["enter", "shift"],
+          action: function() {
             var paramsdata = {
-              app_id : app_id,
-              developer_id : dev_id
-            }
-            // Ajax here 
+              app_id: app_id,
+              developer_id: dev_id
+            };
+            // Ajax here
             let self = this;
             $.ajax({
-              type : "POST",
-              contentType : "application/json",
-              url : "/api/revokeapp",
-              data : JSON.stringify(paramsdata),
-              dataType : 'json',
-              success : function(response) {
-                console.log(response)
-                if(response['statusCode'] == 200){
-                  console.log({status: response['statusCode'] });
-                  self.setContentPrepend('<p class="success">App has been revoked successfully.</p>');
+              type: "POST",
+              contentType: "application/json",
+              url: "/api/revokeapp",
+              data: JSON.stringify(paramsdata),
+              dataType: "json",
+              success: function(response) {
+                console.log(response);
+                if (response["statusCode"] == 200) {
+                  console.log({ status: response["statusCode"] });
+                  self.setContentPrepend(
+                    '<p class="success">App has been revoked successfully.</p>'
+                  );
                 } else {
-                  self.setContentPrepend('<p class="error">There is an error occured during the operation.</p>');
+                  self.setContentPrepend(
+                    '<p class="error">There is an error occured during the operation.</p>'
+                  );
                 }
-                setTimeout(() => {window.location.reload();},1000);
+                setTimeout(() => {
+                  window.location.reload();
+                }, 1000);
               },
-              error : function(e) {
+              error: function(e) {
                 console.log("ERROR: ", e);
               }
             });
             return false;
           }
         },
-        cancel: function () {
-        }
+        cancel: function() {}
       }
     });
   }
 
-  function dialogBlacklist(dev_id,app_id,appname) {
+  function dialogBlacklist(dev_id, app_id, appname) {
     $.confirm({
       backgroundDismiss: true,
-      escapeKey: 'cancel',
-      title: 'Blacklist App',
-      content: '<p>This action will revoke all the app permissions and put it in blacklist.</p>' +
-        '<p>Are you sure you want to blacklist this app?</p>',
+      escapeKey: "cancel",
+      title: "Blacklist App",
+      content:
+        "<p>This action will revoke all the app permissions and put it in blacklist.</p>" +
+        "<p>Are you sure you want to blacklist this app?</p>",
       buttons: {
         yes: {
-          text: 'Yes',
-          btnClass: 'btn-blue',
-          keys: ['enter', 'shift'],
-          action: function () {
+          text: "Yes",
+          btnClass: "btn-blue",
+          keys: ["enter", "shift"],
+          action: function() {
             var paramsdata = {
-              app_id : app_id,
-              developer_id : dev_id
-            }
+              app_id: app_id,
+              developer_id: dev_id
+            };
             let self = this;
             $.ajax({
-              type : "POST",
-              contentType : "application/json",
-              url : "/api/blacklist",
-              data : JSON.stringify(paramsdata),
-              dataType : 'json',
-              success : function(response) {
-                console.log(response)
-                if(response['statusCode'] == 200){
-                  console.log({status: response['statusCode'] });
-                  self.setContentPrepend('<p class="success">App has been blacklisted successfully.</p>');
+              type: "POST",
+              contentType: "application/json",
+              url: "/api/blacklist",
+              data: JSON.stringify(paramsdata),
+              dataType: "json",
+              success: function(response) {
+                console.log(response);
+                if (response["statusCode"] == 200) {
+                  console.log({ status: response["statusCode"] });
+                  self.setContentPrepend(
+                    '<p class="success">App has been blacklisted successfully.</p>'
+                  );
                 } else {
-                  self.setContentPrepend('<p class="error">There is an error occured during the operation.</p>');
+                  self.setContentPrepend(
+                    '<p class="error">There is an error occured during the operation.</p>'
+                  );
                 }
-                setTimeout(() => {window.location.reload();},1000);
+                setTimeout(() => {
+                  window.location.reload();
+                }, 1000);
               },
-              error : function(e) {
+              error: function(e) {
                 console.log("ERROR: ", e);
               }
             });
             return false;
           }
         },
-        cancel: function () {
-        }
+        cancel: function() {}
       }
     });
   }
   /**
    * Autocomplete search
    */
-  function acSubscriberApps() {
+  function autoCompleteSearch(appname) {
+    // console.log('hi');
+    let reqUri = `/api/search`;
+    if (appname) reqUri = `/api/search?appname=${appname}`;
+
     $("#subscriber_apps").autocomplete({
       minLength: 1,
-      source: function (req, res){
+      source: function(req, res) {
         $.ajax({
-          url: "/api/search",
-          success: function (appData) {
-            res(appData['appname']);
+          url: reqUri,
+          success: function(data) {
+            res(data.appname);
           },
-          error: function (e) {
-              console.log("ERROR: ", e);
+          error: function(e) {
+            console.log("ERROR: ", e);
           }
-    
-      });
+        });
       },
       select: (e, item) => {
         if (item) {
           let app_name = item.item.value;
           let current_url = window.location.href;
-          if (current_url.includes('?')) {
-            current_url = current_url.split('?')[0];
+          if (current_url.includes("?")) {
+            current_url = current_url.split("?")[0];
           }
           window.location.href = `${current_url}?appname=${app_name}`;
         }
-
-
-    }
+      }
     });
-
   }
-  
 });
