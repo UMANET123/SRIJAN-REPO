fd7315892dc5bd56a6c785e00eedd7a836731c2b
Valindo Godinho <valindo.godinho@gmail.com>
2019-03-08 16:02:14 +0530

    converted the transaction.model functions to use ORM

--- src/micro-services/auth-service/models/transaction.model.js
+++ src/micro-services/auth-service/models/transaction.model.js
@@ -1,57 +1,50 @@
-const pool = require('../config/db');
-
+const pool = require("../config/db");
+const { TransactionData } = require("../config/models");
 //  validate a transaction
 function validateTransaction(...args) {
-    let [reqParams, callback] = args;
-    let {transaction_id, subscriber_id, app_id} = reqParams;
-     //  create a transaction record
-      (async () => {
-        const client = await pool.connect();
-        try {
-          // insert transaction record
-          let txnRecord = await client.query("SELECT * FROM transaction_data WHERE transaction_id=($1) and uuid=($2) and app_id=($3) and status=($4)", [transaction_id, subscriber_id, app_id, 0]);
-          let txnValid = false;
-          if(txnRecord.rows[0]) {
-            txnValid = true;
-          } 
-          return callback(200, {
-            "is_valid": txnValid
-          });
-
-       } finally {
-          client.release();
-        }
-      })().catch(e =>{
-        console.log(e.stack);
-      } );
-  
-  }
+  let [reqParams, callback] = args;
+  let { transaction_id, subscriber_id, app_id } = reqParams;
+  //  create a transaction record
+  return TransactionData.findOne({
+    where: {
+      transaction_id: transaction_id,
+      uuid: subscriber_id,
+      app_id: app_id,
+      status: 0
+    }
+  }).then(result => {
+    if (result) {
+      return callback(200, { is_valid: true });
+    } else {
+      return callback(200, { is_valid: false });
+    }
+  });
+}
 
 //  invalidate a transaction
 function invalidateTransaction(...args) {
-    let [reqParams,reqBody, callback] = args;
-    let {transaction_id} = reqParams;
-    let {subscriber_id, app_id} = reqBody;
-     // create a transaction record
-    (async () => {
-      const client = await pool.connect();
-      try {
-        // insert transaction record
-        let txnRecord = await client.query("UPDATE transaction_data SET status=($1) WHERE transaction_id=($2) and uuid=($3) and app_id=($4) RETURNING status", [1, transaction_id, subscriber_id, app_id]);
-        // console.log({txn: txnRecord.rows[0]});
-        if(txnRecord.rows[0]) {
-          callback(200, null);
-        }  else {
-          callback(204, null);
-        }
-        return;
-    } finally {
-        client.release();
+  let [reqParams, reqBody, callback] = args;
+  let { transaction_id } = reqParams;
+  let { subscriber_id, app_id } = reqBody;
+  // create a transaction record
+  return TransactionData.update(
+    {
+      status: 1
+    },
+    {
+      where: {
+        transaction_id: transaction_id,
+        uuid: subscriber_id,
+        app_id: app_id
       }
-    })().catch(e =>{
-      console.log(e.stack);
-    } );
-  
-  }
+    }
+  ).then(result => {
+    if (result) {
+      return callback(200, null);
+    } else {
+      return callback(204, null);
+    }
+  });
+}
 
-module.exports = {validateTransaction, invalidateTransaction};
+module.exports = { validateTransaction, invalidateTransaction };
