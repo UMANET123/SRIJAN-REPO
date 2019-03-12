8661d2dddd8c9d2d1a6e465386c548b36f242194
tejomayonline <tejomay.saha@srijan.net>
2019-03-12 13:25:24 +0530

    order feature bugfixed , getconsentlist model query updated for counting records

--- src/micro-services/consent-service/controllers/consent-app-list.controller.js
+++ src/micro-services/consent-service/controllers/consent-app-list.controller.js
@@ -1,16 +1,30 @@
-const {getConsentList} = require('../models/consent.model');
-
+/* jshint esversion:6 */
+const { getConsentList } = require("../models/consent.model");
+//  order types
+const ORDER_TYPES = ["asc", "desc"];
 module.exports = (req, res) => {
-    let {subscriber_id} = req.params;
-    let {limit, page, appname, order} = req.query;
-    order_types = ['asc','desc']
-    if(!order_types.includes(order)){
-        return res.status(400).send({
-            error_code: 'BadRequest',
-            error_message: 'Invalid Parameter'
-        })
+  let { subscriber_id } = req.params;
+  let { limit, page, appname, order } = req.query;
+  if (order) {
+    if (!ORDER_TYPES.includes(order)) {
+      // for invalid order type
+      return res.status(400).send({
+        error_code: "BadRequest",
+        error_message: "Invalid Parameter"
+      });
     }
-    getConsentList(subscriber_id, limit, page, appname,order, (status, response) => {
-        return res.status(status).send(response);
-    });
+  } else {
+    //    default order desc
+    order = ORDER_TYPES[1];
+  }
+  getConsentList(
+    subscriber_id,
+    limit,
+    page,
+    appname,
+    order,
+    (status, response) => {
+      return res.status(status).send(response);
+    }
+  );
 };
