c13398b04727377977190fc96c478d8cbb2cf1d0
tejomayonline <tejomay.saha@srijan.net>
2019-03-12 17:54:23 +0530

    auth helper methods doc addede

--- src/micro-services/auth-service/helpers/add-minute-to-date.js
+++ src/micro-services/auth-service/helpers/add-minute-to-date.js
@@ -1,5 +1,14 @@
-//  pass date as timestamp
-//  return timestamp
-module.exports = function AddMinutesToDate(date, minutes) {
-    return new Date(date.getTime() + minutes*60000);
+/**
+ *
+ *
+ * @param {Date} date  Date Object
+ * @param {number} minutes Minute in numbers
+ * @returns {Date} Date Object
+ * Function will add minutes to passed Date and
+ * Return back
+ */
+function AddMinutesToDate(date, minutes) {
+  return new Date(date.getTime() + minutes * 60000);
 }
+
+module.exports = AddMinutesToDate;
--- src/micro-services/auth-service/helpers/mobile-number.modify.js
+++ src/micro-services/auth-service/helpers/mobile-number.modify.js
@@ -1,5 +1,15 @@
-module.exports = msisdn => {
-    if (msisdn.startsWith('+63')) return msisdn;
-    if (msisdn.startsWith('63')) return `+${msisdn}`;
-    return `+63${msisdn}`;
+/**
+ *
+ *
+ * @param {string} msisdn Mobile Number
+ * @returns {string} Updated Mobile Number
+ * Update and Return back mobile number
+ * with Area Code
+ */
+function updateMobileNumber(msisdn) {
+  if (msisdn.startsWith("+63")) return msisdn;
+  if (msisdn.startsWith("63")) return `+${msisdn}`;
+  return `+63${msisdn}`;
 }
+
+module.exports = updateMobileNumber;
