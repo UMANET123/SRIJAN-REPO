fa6c55511f8dfdd7edd486b9096892ce9f2abb80
tejomayonline <tejomay.saha@srijan.net>
2019-03-20 15:50:08 +0530

    collection name, content updated, run.sh file  newman run script renamed

--- src/micro-services/consent-service/run.sh
+++ src/micro-services/consent-service/run.sh
@@ -6,7 +6,7 @@ then
 fi
 if [ $NODE_ENV == 'test' ]
 then
-    (npm test ; npm run dev&) && npx newman -e Globe.2FA.Development.postman_environment.json run globe.2FA.postman_collection.json
+    (npm test ; npm run dev&) && npx newman -e GlobeConsentService.postman_environment.json run GlobeConsentService.postman_collection.json
 else
     npx nodemon app.js
 fi
