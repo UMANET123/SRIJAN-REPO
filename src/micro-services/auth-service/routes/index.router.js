98f5ee039bc173d707c4447c0a243926a65636bb
tejomayonline <tejomay.saha@srijan.net>
2019-04-04 16:44:54 +0530

    update transaction, invalidate transaction endpoint added

--- src/micro-services/auth-service/routes/index.router.js
+++ src/micro-services/auth-service/routes/index.router.js
@@ -15,6 +15,7 @@ const verifyUserController = require("../controllers/verify-user.controller");
 const invalidateTransaction = require("../controllers/invalidate-transaction.controller");
 const createTransaction = require("../controllers/create-transaction.controller");
 const getTransaction = require("../controllers/get-transaction.controller");
+const updateTransaction = require("../controllers/update-transaction.controller.js");
 
 router.post(`/generate/hotp`, generateHotpController);
 router.post(`/generate/totp`, generateTotpController);
@@ -24,7 +25,7 @@ router.post(`/verify/user`, verifyUserController);
 
 //  transaction routes
 router.post("/transaction", createTransaction);
-// router.put("/transaction/:transaction_id", updateTransaction);
+router.put("/transaction/:transaction_id", updateTransaction);
 router.get("/transaction/:transaction_id", getTransaction);
 router.put(`/transaction/:transaction_id/invalidate`, invalidateTransaction);
 module.exports = router;
