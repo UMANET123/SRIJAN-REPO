dea3096ee9e4940aea3fd058cf54a7be950d9387
Valindo Godinho <valindo.godinho@gmail.com>
2019-04-05 13:03:43 +0530

    Added consent skip logic, also accomodated for consent expiry but no logic in place

--- src/micro-services/consent-service/models/skip-consent.model.js
+++ src/micro-services/consent-service/models/skip-consent.model.js
@@ -1,37 +1,87 @@
 const { SubscriberConsent } = require("../config/models");
-function skipConsent(params, callback) {
-  let { uuid, app_id } = params;
-  // scopes = JSON.parse(scopes).sort();
-  SubscriberConsent.findOne({
-    where: {
-      uuid: uuid,
-      app_id: app_id,
-    },
-    attributes: ["uuid", "app_id", "developer_id", "access_token", "scopes", "status"]
-  }).then(result => {
-    if (result) {
-      // retrivedScopes = result.scopes.sort();
-      // console.log(retrivedScopes)
-      // if (retrivedScopes.length == scopes.length) {
-      //   for (var i = 0; i < scopes.length; i++) {
-      //     if (scopes[i] != retrivedScopes[i]) {
-      //       return callback(false, 200);
-      //     }
-      //   }
-      //   return callback(true, 200);
-      // } else {
-      //   return callback(false, 200);
-      // }
-      console.log(result)
-      if(result.access_token!=null && result.scopes !=null && result.status == 0){
-        return callback({status:true, scopes: result.scopes}, 200)
+/**
+ *
+ * Status Codes:  -1, 0, 1
+ * -1 = Always Skip
+ * 0 = Allow Skip
+ * 1 = Always Ask
+ */
+/**
+ * This function will return whether the application can skip consent or not,
+ * based on certain factors as follows:
+ * -status code
+ * -expiry
+ * -scopes match or mismatch
+ * @param {Object} req
+ * @param {Function} callback
+ * @returns {callback({status: boolean}, statusCode)}
+ * Will return callback with status object and http status code
+ */
+
+function skipConsent(req, callback) {
+  // PLEASE NOT `subscriber_id` is the same as `uuid`
+  let { uuid, app_id } = req.params;
+  let { expiry, scopes, developer_id, status } = req.query;
+  let returnStatus = false;
+  /**
+   * Parse Scopes only if they exist
+   */
+  if(scopes) scopes = JSON.parse(scopes).sort();
+  if (status == -1) {
+    returnStatus = true;
+    return callback({ status: returnStatus }, 200);
+  } else if (status == 1) {
+    returnStatus = false;
+    return callback({ status: returnStatus }, 200);
+  } else {
+    SubscriberConsent.findOne({
+      where: {
+        uuid: uuid,
+        app_id: app_id,
+        developer_id: developer_id
+      },
+      attributes: [
+        "uuid",
+        "app_id",
+        "developer_id",
+        "access_token",
+        "scopes",
+        "status"
+      ]
+    }).then(result => {
+      if (result) {
+        if (scopes) {
+          //Scopes from the database record
+          resultScopes = result.scopes.map(scope => scope.toLowerCase());
+          if (resultScopes.length < scopes.length) {
+            returnStatus = false;
+          } else {
+            /**
+             * Setting the returnStatus = true, incase scopes sent is the subset of
+             * resultScopes ( Scopes returned from db)
+             * During the map, if any scope is found out of place, returnStatus is set to false
+             */
+            returnStatus = true;
+            scopes.map(scope => {
+              if (!resultScopes.includes(scope.toLowerCase())) {
+                returnStatus = false;
+              }
+            });
+          }
+        } else {
+          // Skip if no scopes are present
+          returnStatus = true;
+        }
       } else {
-        return callback({status:false},200);
+        // Don't skip if the record is not present
+        returnStatus = false;
       }
-    } else {
-      return callback({status: false}, 200);
-    }
-  });
+    }).finally(()=>{
+      return callback({ status: returnStatus }, 200);
+    });
+    
+    
+  }
 }
 
 module.exports = { skipConsent };
